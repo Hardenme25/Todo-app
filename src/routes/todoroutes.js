5a59b2742b82be22cd8dec4fae53e23a85509f57
Kayongo Emmanuel <kayongoemma3@gmail.com>
2026-06-22 17:12:32 +0300

    Refactor todo routes to use Prisma for database operations

--- src/routes/todoroutes.js
+++ src/routes/todoroutes.js
@@ -8,66 +8,78 @@ import prisma from "../../prisma/prismaconfig.js";
 const router = express.Router();
 
 // Handle a get request to get the default todos
-router.get("/", (req,res) => {
+router.get("/",  async (req,res) => {
     // Get the todos 
-    const getTodos = db.prepare(`
-        SELECT * FROM todos WHERE  user_id = ?   
-    `);
-    // Get the todos where the userid matches the one in the token.
-    const todos = getTodos.all(req.user_id);
-
+    const getTodos = await prisma.todos.findMany(
+        {
+            where: {
+                user_id: req.user_id
+            }
+        }
+    );
     // Now send the todos as a response 
-    res.status(200).json(todos);
+    res.status(200).json(getTodos);
 });
 
 // Handle a request for adding a todo
-router.post("/", (req,res) => {
+router.post("/", async (req,res) => {
     // Get the todo from the request of the body
     const { task } = req.body;
 
     // Add the task to the todos table
-    const new_todo = db.prepare(`
-        INSERT INTO todos (task,user_id ) VALUES (?,?)
-    `);
-    // Run the sql query.
-    const result = new_todo.run(task,req.user_id);
+    const new_todo = await prisma.todos.create(
+        {
+            data: {
+                task: task,
+                user_id: req.user_id
+            }
+        }
+    );
 
     // Send the todo to the user
     res.json(new_todo);
 });
 
 // Handle a request for completing a todo
-router.put("/:id", (req,res) => {
+router.put("/:id", async (req,res) => {
     // Get the id from the request params since a single user can have multiple todos.
     const { id } = req.params;
     // Get the user_id from the request body
     const userid = req.user_id;
     
     // Update the todo to be completed
-    const update_todo = db.prepare(`
-        UPDATE todos SET completed = 1 WHERE id = ? AND user_id = ?
-    `);
-    // Run the sql query.
-    const result = update_todo.run(id,userid);
+    const update_todo = await prisma.todos.update(
+        {
+            where: {
+                id: id,
+                user_id: userid
+            },
+            data: {
+                completed: 1
+            }
+        }
+    );
 
     // Send the todo to the user
     res.json({message: "Task completed!"});
 });
 
 // Handle a request for deleting a todo
-router.delete("/:id", (req,res) => {
+router.delete("/:id", async (req,res) => {
     // Get the id from the request params since a single user can have multiple todos.
     const { id } = req.params;
     // Get the user_id from the request body
     const userid = req.user_id;
     
     // Delete the todo
-    const delete_todo = db.prepare(`
-        DELETE FROM todos WHERE id = ? AND user_id = ?
-    `);
-    // Run the sql query.
-    const result = delete_todo.run(id,userid);
-
+    const delete_todo = await prisma.todos.delete(
+        {
+            where: {
+                id: id,
+                user_id: userid
+            }
+        }
+    );
     // Send the todo to the user
     res.json({message: "Task deleted!"});
 });
