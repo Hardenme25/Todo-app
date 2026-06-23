76298160960884659d3f67591e45d8cd18ef99cd
Kayongo Emmanuel <kayongoemma3@gmail.com>
2026-06-23 13:10:57 +0300

    Remeve the prisma configuration file from the Prisma folder

--- prisma/prismaconfig.js
+++
@@ -1,8 +0,0 @@
-// Importing the Prisma Client instance from the generated Prisma client package
-import { prismaClient } from "@prisma/client";
-
-// Creating an install to be exported
-const prisma = new prismaClient();
-
-// Exporting the prisma instance to be used in other parts of the application
-export default prisma;
